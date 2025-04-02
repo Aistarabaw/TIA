9db18fe6da4e9a6c9b211f7776d12ed88a8eead7
Inês Marques <a100434@alunos.uminho.pt>
2025-04-03 00:19:08 +0100

    Primeiro commit

---
+++ MenuProjeto.pl
@@ -0,0 +1,30 @@
+:- set_prolog_flag(encoding, utf8).
+:- encoding(utf8).
+menu :-
+    repeat,
+    limpar_consola,
+    write('--------------------------------------------------------------'), nl,
+    write('|          Tecnologias de Inteligência Artificial           |'), nl,
+    write('|                         LEGSI                              |'), nl,
+    write('--------------------------------------------------------------'), nl, nl,
+    write('[Menu]'), nl, nl,
+    write('[1] - Paciente do sexo feminino'), nl,
+    write('[2] - Paciente do sexo masculino'), nl, nl,
+    write('Selecione o algarismo referente à sua escolha: '),
+    read(Opcao),
+    executar(Opcao),
+    Opcao == 3, !.
+
+executar(1) :-
+    write('Você escolheu a Opção 1.'), nl, fail.
+executar(2) :-
+    write('Você escolheu a Opção 2.'), nl, fail.
+executar(3) :-
+    write('Saindo...'), nl.
+executar(_) :-
+    write('Opção inválida, tente novamente.'), nl, fail.
+
+
+limpar_consola :- 
+    forall(between(1, 50, _), nl).
+    
